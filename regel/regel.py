60af19368f1864645c2675712f80d9f202f0c92c
Peter van der Heijden <peter@vanderheijden.name>
2021-10-28 19:58:41 +0200

    Move regel to separate repository and create some package structure.

---
+++ regel/regel.py
@@ -0,0 +1,171 @@
+import re
+import sys
+from parsec import ParseError, digit, generate, letter, many, none_of, string
+import operator
+from functools import partial, reduce
+
+_MODULE = "module"
+
+
+def regel(typename, pattern):
+    def _init(self, text):
+        match = self._regex.match(text)
+        if not match:
+            raise ValueError(
+                f"Text '{text}' does not match pattern '{self._pattern}'")
+        strings = match.groups()
+        values = [
+            self._apply_many(funcs, string)
+            for funcs, string
+            in zip(self._funcLists, strings)
+        ]
+        for field, value in zip(self._fields, values):
+            setattr(self, field, value)
+
+    def _apply_many(self, funcs, string):
+        return reduce(self._apply, funcs, string)
+
+    def _apply(self, value, applFunc):
+        application, func = applFunc
+        return func(value) if application == ':' else [func(elem) for elem in value]
+
+    try:
+        regex, fields, funcLists = _regel.parse_strict(pattern)
+    except ParseError as err:
+        raise ValueError(
+            f"Error parsing pattern '{pattern}' at position {err.loc()}.")
+
+    seen = set()
+    for field in fields:
+        if field in seen:
+            raise ValueError(f"Duplicate field '{field}'.")
+        seen.add(field)
+
+    try:
+        caller = sys._getframe(1)
+        f_globals = dict(caller.f_globals)
+        f_locals = dict(caller.f_locals)
+        module = f_globals.get('__name__', '__main__')
+    except (AttributeError, ValueError):
+        f_globals = {}
+        f_locals = {}
+        module = __name__
+
+    funcLists = [
+        [
+            (application, eval(func, f_globals, f_locals))
+            for (application, func)
+            in funcs
+        ]
+        for funcs in funcLists
+    ]
+
+    namespace = {
+        "__module__": module,
+        "__init__": _init,
+        "_apply_many": _apply_many,
+        "_apply": _apply,
+        "_regex": re.compile(regex),
+        "_fields": fields,
+        "_funcLists": funcLists,
+        "_parse": _parse,
+        "_pattern": pattern,
+    }
+
+    return type(typename, (), namespace)
+
+
+def eq(value):
+    return partial(operator.eq, value)
+
+
+def ne(value):
+    return partial(operator.ne, value)
+
+
+def split(*args):
+    delimiters = args or [' ']
+
+    def _split(value):
+        result = [value]
+        for delimiter in delimiters:
+            result = [
+                string
+                for fragment in result
+                for string in fragment.split(delimiter)
+            ]
+        return result
+
+    return _split
+
+
+def const(value):
+    return lambda _: value
+
+
+@classmethod
+def _parse(cls, text):
+    return cls(text)
+
+
+@generate
+def _regel():
+    head = yield _text
+    tail = yield many(_field_with_funcs + _text)
+    regex = "(.*)".join([head, *[re.escape(t) for _, t in tail]])
+    fields = [f[0] for f, _ in tail]
+    funcs = [f[1] for f, _ in tail]
+    return regex, fields, funcs
+
+
+@generate
+def _text():
+    chars = yield many(_open_brace ^ _close_brace ^ _backslash ^ none_of("{}"))
+    return "".join(chars)
+
+
+@generate
+def _open_brace():
+    yield string("\{")
+    return "{"
+
+
+@generate
+def _close_brace():
+    yield string("\}")
+    return "}"
+
+
+@generate
+def _backslash():
+    yield string("\\\\")
+    # A backslash still needs to be escaped in the regex.
+    return "\\\\"
+
+
+@generate
+def _func():
+    chars = yield many(_open_brace ^ _close_brace ^ _backslash ^ _colon ^ none_of("{:}"))
+    return "".join(chars)
+
+
+@generate
+def _colon():
+    yield string("\:")
+    return ":"
+
+
+@generate
+def _field_with_funcs():
+    yield string("{")
+    identifier = yield _identifier
+    funcs = yield many((string("::") ^ string(":")) + _func)
+    yield string("}")
+    return identifier, funcs
+
+
+@generate
+def _identifier():
+    head = yield letter()
+    tail = yield many(string("_") | letter() | digit())
+    return head + "".join(tail)
---
+++ regel/test_regel.py
@@ -0,0 +1,180 @@
+import pytest
+from .regel import const, eq, ne, regel, split
+
+
+def test_string_field():
+    obj = regel("Obj", "{field}")._parse("value")
+    assert obj.field == "value"
+
+
+def test_string_field_begin():
+    obj = regel("Obj", "{field}xxx")._parse("valuexxx")
+    assert obj.field == "value"
+
+
+def test_string_field_middle():
+    obj = regel("Obj", "xxx{field}xxx")._parse("xxxvaluexxx")
+    assert obj.field == "value"
+
+
+def test_string_field_end():
+    obj = regel("Obj", "xxx{field}")._parse("xxxvalue")
+    assert obj.field == "value"
+
+
+def test_group_in_pattern():
+    obj = regel("Obj", "{field} (abc)")._parse("value (abc)")
+    assert obj.field == "value"
+
+
+def test_2015_day13_seating():
+    Seating = regel(
+        "Seating",
+        "{guest1} would {gain:eq('gain')} {happiness:int} happiness units by sitting next to {guest2}."
+    )
+    seating = Seating._parse(
+        "Alice would gain 54 happiness units by sitting next to Bob.")
+    assert seating.guest1 == "Alice"
+    assert seating.gain == True
+    assert seating.happiness == 54
+    assert seating.guest2 == "Bob"
+
+
+def test_local_function():
+    def f(x):
+        return 1 + int(x)
+    obj = regel("Obj", "The number is {number:f}.")._parse("The number is 5.")
+    assert obj.number == 6
+
+
+def test_dictionary():
+    obj = regel("Obj", "{dict:lambda x\: \{x\: 5\}}")._parse("five")
+    assert "five" in obj.dict
+    assert obj.dict["five"] == 5
+
+
+def test_braces_in_text():
+    obj = regel("Obj", "\{not_a_field:int\} {a_field:int}")._parse(
+        "{not_a_field:int} 42")
+    assert obj.a_field == 42
+
+
+def test_module_name():
+    Obj = regel("Obj", "{field} (abc)")
+    assert Obj.__module__ == __name__
+
+
+def test_constructor():
+    Obj = regel("Obj", "{field1:int} {field2:int}")
+    obj = Obj("42 43")
+    assert obj.field1 == 42
+    assert obj.field2 == 43
+
+
+def test_field_cannot_start_with_underscore():
+    with pytest.raises(ValueError):
+        regel("Obj", "{_field}")
+
+
+def test_do_not_use_str_for_no_conversion():
+    # Initially str() was used as a no-op string conversion.
+    # Which has the below potential problem.
+    str = int
+    obj = regel("Obj", "{field1}")._parse("42")
+    assert obj.field1 == "42"
+
+
+def test_duplicate_field():
+    with pytest.raises(ValueError):
+        regel("Obj", "{field} {field}")
+
+
+def test_split_default_separator():
+    obj = regel("Obj", "{fields:split()}.")._parse("one two three.")
+    assert obj.fields == ["one", "two", "three"]
+
+
+def test_split_no_match():
+    obj = regel("Obj", "{fields:split(',')}.")._parse("one two three.")
+    assert obj.fields == ["one two three"]
+
+
+def test_split_multiple_delimiters():
+    obj = regel("Obj", "{fields:split(', ', ' and ')}.")._parse(
+        "one, two and three.")
+    assert obj.fields == ["one", "two", "three"]
+
+
+def test_list_to_ints():
+    obj = regel("Obj", "{fields:split()::int}")._parse("1 2 3")
+    assert obj.fields == [1, 2, 3]
+
+
+def test_list_to_ints_to_bools():
+    obj = regel("Obj", "{fields:split()::int::eq(2)}")._parse("1 2 3")
+    assert obj.fields == [False, True, False]
+
+
+def test_list_to_ints_to_bools_to_len():
+    obj = regel("Obj", "{fields:split()::int::eq(2):len}")._parse("1 2 3")
+    assert obj.fields == 3
+
+
+def test_field_to_list():
+    obj = regel("Obj", "{fields::str}")._parse("abc")
+    assert obj.fields == ["a", "b", "c"]
+
+
+def test_colon_in_text():
+    obj = regel("Obj", "The value is: {field}")._parse("The value is: 42")
+    assert obj.field == "42"
+
+
+def test_backslash_in_text():
+    obj = regel("Obj", r"The path is: {pathname}")._parse(
+        r"The path is: c:\temp\readme.txt")
+    assert obj.pathname == r"c:\temp\readme.txt"
+
+
+def test_backslash_before_open_brace():
+    obj = regel("Obj", r"The path is: c:\\temp\\{filename}")._parse(
+        r"The path is: c:\temp\readme.txt")
+    assert obj.filename == "readme.txt"
+
+
+def test_2020_day7_contains_other():
+    class Contents(regel('Contents', "{number:int} {c1} {c2} bag")):
+        def __init__(self, row):
+            super().__init__(row)
+            self.color = f"{self.c1} {self.c2}"
+
+    class Bag(regel('Bag', "{color} bags contain {contents:split(', ', 's, ')::Contents}.")):
+        pass
+
+    bag = Bag("light red bags contain 1 bright white bag, 2 muted yellow bags.")
+
+    assert bag.color == "light red"
+    assert len(bag.contents) == 2
+    assert bag.contents[0].number == 1
+    assert bag.contents[0].color == "bright white"
+    assert bag.contents[1].number == 2
+    assert bag.contents[1].color == "muted yellow"
+
+
+def test_2020_day7_contains_other():
+    class Bag(regel('Bag', "{color} bags contain no {contents:const([])}.")):
+        pass
+
+    bag = Bag("faded blue bags contain no other bags.")
+
+    assert bag.color == "faded blue"
+    assert len(bag.contents) == 0
+
+
+@pytest.mark.skip("TODO")
+def test_2020_day7_bags_with_and_without_contents():
+    pass
+
+# 2020 day 4, passport, dictionary
+# 2020 day 7, shiny gold bag, list, object
+# 2020 day 14, bitmask program
